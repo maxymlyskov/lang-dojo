fe6bfbdee3f1502a833b0740d668b7ec3c043f18
maxymlyskov <raypember02@gmail.com>
2023-05-16 18:07:00 +0300

    Implemneted footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,25 @@
-import * as React from 'react'
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import List from '@mui/material/List'
-import Typography from '@mui/material/Typography'
-import ListItem from '@mui/material/ListItem'
-import IconButton from '@mui/material/IconButton'
-import Button from '@mui/material/Button'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
 
 //IMPORTING SVG'S AND ICONS
-import InstagramIcon from '@mui/icons-material/Instagram'
-import TwitterIcon from '@mui/icons-material/Twitter'
-import FacebookIcon from '@mui/icons-material/Facebook'
-import logo from '../../assets/logo.svg'
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import logo from "../../assets/logo.svg";
 
-import styles from './styles'
+import styles from "./styles";
 
-const company = ['about', 'Parteners', 'careers', 'Teaching Center']
-const categories = ['Classroom courses', 'Video Courses', 'Offline Courses']
-const support = ['contact us', 'site feedback']
-const getInTouch = ['Privacy', 'Help', 'Contact']
-const navItems = ['About', 'Careers', 'Resource center', 'Guides']
+const company = ["about", "Parteners", "careers", "Teaching Center"];
+const categories = ["Classroom courses", "Video Courses", "Offline Courses"];
+const support = ["contact us", "site feedback"];
+const getInTouch = ["Privacy", "Help", "Contact"];
+const navItems = ["About", "Careers", "Resource center", "Guides"];
 
 const Footer = () => {
   return (
@@ -27,50 +27,50 @@ const Footer = () => {
       <Box sx={styles.wrapper}>
         <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={6} sm={4} md={3}>
-            <Typography variant='h3'>Company</Typography>
+            <Typography variant="h3">Company</Typography>
             <List>
               {company.map((item, index) => {
                 return (
                   <ListItem sx={styles.itemList} key={index}>
                     {item}
                   </ListItem>
-                )
+                );
               })}
             </List>
           </Grid>
           <Grid item xs={6} sm={4} md={3}>
-            <Typography variant='h3'>categories</Typography>
+            <Typography variant="h3">categories</Typography>
             <List>
               {categories.map((item, index) => {
                 return (
                   <ListItem sx={styles.itemList} key={index}>
                     {item}
                   </ListItem>
-                )
+                );
               })}
             </List>
           </Grid>
           <Grid item xs={6} sm={4} md={3}>
-            <Typography variant='h3'>support</Typography>
+            <Typography variant="h3">support</Typography>
             <List>
               {support.map((item, index) => {
                 return (
                   <ListItem sx={styles.itemList} key={index}>
                     {item}
                   </ListItem>
-                )
+                );
               })}
             </List>
           </Grid>
           <Grid item xs={6} sm={4} md={3}>
-            <Typography variant='h3'>get in touch</Typography>
+            <Typography variant="h3">get in touch</Typography>
             <List>
               {getInTouch.map((item, index) => {
                 return (
                   <ListItem sx={styles.itemList} key={index}>
                     {item}
                   </ListItem>
-                )
+                );
               })}
             </List>
             <Box sx={styles.links}>
@@ -88,7 +88,7 @@ const Footer = () => {
         </Grid>
         <Box sx={styles.copyrightBlock}>
           <Typography sx={styles.logo}>
-            <Box component='img' src={logo} />
+            <Box component="img" src={logo} />
           </Typography>
           <Typography sx={styles.copyrightText}>
             Copyright © 2022 Turitor. All Right Reserved.
@@ -99,13 +99,13 @@ const Footer = () => {
                 <Button key={index} sx={styles.linkBtn}>
                   {item}
                 </Button>
-              )
+              );
             })}
           </Box>
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
